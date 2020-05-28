9fc97901ff52d43cdfd7ed5aaefc6d657fade6f3
vinni06 <58682029+vinni06@users.noreply.github.com>
2020-05-28 16:56:52 +0530

    Update setup.sh

--- src/main/scripts/setup.sh
+++ src/main/scripts/setup.sh
@@ -1,7 +1,7 @@
-kubectl create -f deployment-todo.yml
-kubectl create -f deployment-user.yml
+microk8s.kubectl create -f deployment-todo.yml
+microk8s.kubectl create -f deployment-user.yml
 
-kubectl create -f service-todo.yml
-kubectl create -f service-user.yml
+microk8s.kubectl create -f service-todo.yml
+microk8s.kubectl create -f service-user.yml
 
-kubectl create -f ingress.yml
+microk8s.kubectl create -f ingress.yml
